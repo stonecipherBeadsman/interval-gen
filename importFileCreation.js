dab46290914aee0e19cfd6f3278b20dc3b726572
Josiah Wetzel <Josiah.Wetzel@nisc.coop>
2017-11-17 13:05:03 -0600

    Added more granularity for the shape of the pseudorandom data

--- importFileCreation.js
+++ importFileCreation.js
@@ -293,12 +293,26 @@
         }
         if(hourOfTheDay <= 5 * intervalMultiplier){
             return randomIntFromInterval(getScaledIntervalBound(0, 1, intervalMultiplier), getScaledIntervalBound(0.25, multiplier, intervalMultiplier));
+        }else if(hourOfTheDay <= 6 * intervalMultiplier){
+            return randomIntFromInterval(getScaledIntervalBound(0.2, 1, intervalMultiplier), getScaledIntervalBound(0.75, multiplier, intervalMultiplier));
+        }else if(hourOfTheDay <= 7 * intervalMultiplier){
+            return randomIntFromInterval(getScaledIntervalBound(0.2, 1, intervalMultiplier), getScaledIntervalBound(1, multiplier, intervalMultiplier));
+        }else if(hourOfTheDay <= 8 * intervalMultiplier){
+            return randomIntFromInterval(getScaledIntervalBound(0.25, 1, intervalMultiplier), getScaledIntervalBound(2, multiplier, intervalMultiplier));
         }else if(hourOfTheDay <= 10 * intervalMultiplier){
-            return randomIntFromInterval(getScaledIntervalBound(0.25, 1, intervalMultiplier), getScaledIntervalBound(3.5, multiplier, intervalMultiplier));
-        }else if(hourOfTheDay <= 15 * intervalMultiplier){
+            return randomIntFromInterval(getScaledIntervalBound(0.5, 1, intervalMultiplier), getScaledIntervalBound(2, multiplier, intervalMultiplier));
+        }else if(hourOfTheDay <= 11 * intervalMultiplier){
+            return randomIntFromInterval(getScaledIntervalBound(0.25, 1, intervalMultiplier), getScaledIntervalBound(1.75, multiplier, intervalMultiplier));
+        }else if(hourOfTheDay <= 12 * intervalMultiplier){
             return randomIntFromInterval(getScaledIntervalBound(0.25, 1, intervalMultiplier), getScaledIntervalBound(2,  multiplier, intervalMultiplier));
+        }else if(hourOfTheDay <= 13 * intervalMultiplier){
+            return randomIntFromInterval(getScaledIntervalBound(0.75, 1, intervalMultiplier), getScaledIntervalBound(1.75,  multiplier, intervalMultiplier));
+        }else if(hourOfTheDay <= 14 * intervalMultiplier){
+            return randomIntFromInterval(getScaledIntervalBound(0.65, 1, intervalMultiplier), getScaledIntervalBound(1.5,  multiplier, intervalMultiplier));
+        }else if(hourOfTheDay <= 15 * intervalMultiplier){
+            return randomIntFromInterval(getScaledIntervalBound(0.85, 1, intervalMultiplier), getScaledIntervalBound(2.5,  multiplier, intervalMultiplier));
         }else if(hourOfTheDay <= 20 * intervalMultiplier){
-            return randomIntFromInterval(getScaledIntervalBound(2, 1, intervalMultiplier), getScaledIntervalBound(6, multiplier, intervalMultiplier));
+            return randomIntFromInterval(getScaledIntervalBound(0.75, 1, intervalMultiplier), getScaledIntervalBound(3.5, multiplier, intervalMultiplier));
         }else{
             return randomIntFromInterval(getScaledIntervalBound(0.05, 1, intervalMultiplier), getScaledIntervalBound(1.25, multiplier, intervalMultiplier));
         }
